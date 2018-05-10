9645ac2959f1ba0de69bc6de9861ca304b85d202
Dara  Yiu <darayiu@D-10-18-238-62.dhcp4.washington.edu>
2018-05-10 12:49:25 -0700

    changed name of script to fish-analysis

---
+++ fish-analysis.R
@@ -0,0 +1,35 @@
+fish_data <- read.csv("data/Gaeta_etal_CLC_data.csv")
+
+library(tidyverse)
+
+#create columns of categorical data dividng fish by length into big and small
+fish_data_cat = fish_data %>%
+  mutate(length_cat = ifelse(length > 200, "big", "small"))
+summary(fish_data_cat)
+
+#filter out innacurate observations where scale length is less than 1mm
+descaled_fish_data_cat <- fish_data_cat %>%
+  group_by(scalelength) %>%
+  filter(scalelength > 1)
+summary(descaled_fish_data_cat)
+View(descaled_fish_data_cat)
+
+#plot histogram of fish length by size 
+ggplot(descaled_fish_data_cat, aes(x = scalelength, fill = length_cat)) +
+  geom_histogram()
+
+#Plot histogram of scale length by fish categorical size
+scale_hist_by_length <- ggplot(fish_data_cat, aes(x = scalelength, fill = length_cat)) +
+  geom_histogram()
+
+#save scale length figure
+ggsave("figures/scale_hist_by_lenth.jpeg")
+
+#scatterplot
+ggplot(data = descaled_fish_data_cat, aes(x=length, y=scalelength, color = lakeid)) + 
+       geom_point() +
+  ylab("Fish scale length (mm)" ) +
+  xlab("Fish standard length (mm)") +
+  theme_classic()
+
+

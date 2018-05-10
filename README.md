626db49652fc68d5cf91069062a65db2b190880f
Dara  Yiu <darayiu@D-10-18-238-62.dhcp4.washington.edu>
2018-05-10 10:48:12 -0700

    clean up script & edit ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # DaraYiu-1
+
+**Packages** 
+library(tidyverse) - includes ggplot, dplyr
+
+**Data** 
+data/Gaeta_etal_CLC_data.csv - fish length and scale data
+
+**Figures** 
+scale_hist_by_length.jpeg 
+![figures](figures/scale_hist_by_length.jpeg)
+
+**Scripts** 
+scripts.R - contains all script with fish data for practicing Git version control
--- script.R
+++ script.R
@@ -1,17 +1,27 @@
 fish_data <- read.csv("data/Gaeta_etal_CLC_data.csv")
-library(dplyr)
-library(ggplot2)
+
 library(tidyverse)
 
+#create columns of categorical data dividng fish by length into big and small
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 200, "big", "small"))
+summary(fish_data_cat)
 
-ggplot(fish_data_cat, aes(x = scalelength, fill = length_cat)) +
+#filter out innacurate observations where scale length is less than 1mm
+descaled_fish_data_cat <- fish_data_cat %>%
+  group_by(scalelength) %>%
+  filter(scalelength > 1)
+summary(descaled_fish_data_cat)
+
+#plot histogram of fish length by size 
+ggplot(descaled_fish_data_cat, aes(x = scalelength, fill = length_cat)) +
   geom_histogram()
-
 
 #Plot histogram of scale length by fish categorical size
 scale_hist_by_length <- ggplot(fish_data_cat, aes(x = scalelength, fill = length_cat)) +
   geom_histogram()
 
+#save scale length figure
 ggsave("figures/scale_hist_by_lenth.jpeg")
+
+

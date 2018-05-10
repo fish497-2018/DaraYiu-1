6c0f9de6720466488e3ccfa11747c579f948efce
Dara  Yiu <darayiu@D-10-18-238-62.dhcp4.washington.edu>
2018-05-10 09:48:02 -0700

    create histogram with scale length

--- script.R
+++ script.R
@@ -1,5 +1,8 @@
 fish_data <- read.csv("data/Gaeta_etal_CLC_data.csv")
 library(dplyr)
+library(ggplot2)
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 200, "big", "small"))
 
+ggplot(fish_data_cat, aes(x = scalelength, fill = length_cat)) +
+  geom_histogram()
